93fb905b291ffcbf5c06a580a09097ddf1407786
MavenPH <priyanka.hore@nciportal.com>
2024-05-29 15:51:25 +0530

    some changes

--- src/main/java/com/demoMaven/First_Project/App.java
+++ src/main/java/com/demoMaven/First_Project/App.java
@@ -9,14 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("Hello");
-        System.out.println("Good morning!");
-
-        System.out.println( "Hello World!" );
-        System.out.println("Hello");
-        System.out.println("Good morning!");
-
-        System.out.println("fsfasd!");
+        System.out.println("Good evening!");
 
     }
 }
